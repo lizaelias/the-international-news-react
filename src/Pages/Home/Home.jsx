f6244b0525fa0b8c4e1fa55fbc0766bdde363a13
lizaelias <elias221624@gmail.com>
2024-03-04 15:09:14 +0600

    create a navbar and responsive webside

--- src/Pages/Home/Home.jsx
+++ src/Pages/Home/Home.jsx
@@ -1,12 +1,13 @@
 import React from "react";
 import Header from "../../Sheard/Header/Header";
 import Marquee from "react-fast-marquee";
+import Navbar from "../../Sheard/Navbar/Navbar";
 
 const Home = () => {
   return (
-    <div>
+    <div className="px-2">
       <Header></Header>
-      <div className="flex bg-[#F3F3F3] p-2 mt-4">
+      <div className="flex bg-[#F3F3F3] p-2 mt-4 items-center">
         <button className="btn rounded-none btn-secondary">Latest</button>
         <Marquee>
           <a className="text-xl font-bold" href="">Match Highlights: Germany vs Spain — as it happened   !   Match Highlights: Germany vs Spain as...</a>
@@ -14,6 +15,8 @@ const Home = () => {
           <a className="text-xl font-bold" href="">Match Highlights: Germany vs Spain — as it happened   !   Match Highlights: Germany vs Spain as...</a>
         </Marquee>
       </div>
+
+      <Navbar></Navbar>
     </div>
   );
 };
--- src/Sheard/Navbar/Navbar.jsx
+++ src/Sheard/Navbar/Navbar.jsx
@@ -1,11 +1,64 @@
-import React from 'react';
+import React from "react";
+import { Link } from "react-router-dom";
+import user from '../../../public/assets/user.png'
 
 const Navbar = () => {
-    return (
-        <div>
-            
+
+  const navbar = <> 
+                     <li className="text-xl font-medium"><Link to='/'>Home</Link></li>
+                    <li className="text-xl font-medium"><Link to='/about'>Abou</Link></li>
+                    <li className="text-xl font-medium"><Link to='/career'>Career</Link></li>
+
+                </>
+   
+  
+ 
+  
+  return (
+    <div>
+      <div className="navbar mt-5">
+        <div className="navbar-start">
+          <div className="dropdown">
+            <div tabIndex={0} role="button" className="btn btn-ghost lg:hidden">
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                className="h-5 w-5"
+                fill="none"
+                viewBox="0 0 24 24"
+                stroke="currentColor"
+              >
+                <path
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                  strokeWidth="2"
+                  d="M4 6h16M4 12h8m-8 6h16"
+                />
+              </svg>
+            </div>
+            <ul
+              tabIndex={0}
+              className="menu menu-sm dropdown-content mt-3 z-[1] p-2 shadow bg-base-100 rounded-box w-52"
+            >
+             {
+              navbar
+             }
+            </ul>
+          </div>
+         
         </div>
-    );
+        <div className="navbar-center hidden lg:flex">
+          <ul className="menu menu-horizontal px-1">
+            {navbar}
+          </ul>
+        </div>
+        
+        <div className="navbar-end">
+        <img className="w-12" src={user} alt="" />
+          <button className="btn bg-[#403F3F] text-white px-10 py-3 font-bold ml-2">Login</button>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Navbar;
